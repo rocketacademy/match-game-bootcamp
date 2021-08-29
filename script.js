c902304318a40b977095e8fb5eb70a55c5d60497
Kimmi Lee <lee.kimmixq@gmail.com>
2021-08-30 01:38:57 +0900

    show suit when turn over

--- script.js
+++ script.js
@@ -4,54 +4,42 @@ const board = [];
 let firstCard = null;
 let firstCardElement;
 let deck;
+let canClick = true;
 
 const squareClick = (cardElement, column, row) => {
   console.log(cardElement);
-
   console.log('FIRST CARD DOM ELEMENT', firstCard);
-
   console.log('BOARD CLICKED CARD', board[column][row]);
 
   const clickedCard = board[column][row];
 
   // the user already clicked on this square
-  if (cardElement.innerText !== '') {
+  if (cardElement.innerText !== '' || canClick === false) {
     return;
   }
 
-  // first turn
   if (firstCard === null) {
     console.log('first turn');
     firstCard = clickedCard;
-    // turn this card over
-    cardElement.innerText = firstCard.name;
-
-    // hold onto this for later when it may not match
+    cardElement.innerText = `${firstCard.suit}${firstCard.name}`;
     firstCardElement = cardElement;
-
-    // second turn
   } else {
     console.log('second turn');
+    cardElement.innerText = `${clickedCard.suit}${clickedCard.name}`;
+    canClick = false;
     if (
       clickedCard.name === firstCard.name
         && clickedCard.suit === firstCard.suit
     ) {
       console.log('match');
-
-      // turn this card over
-      cardElement.innerText = clickedCard.name;
     } else {
       console.log('NOT a match');
-
-      // turn this card back over
-      cardElement.innerText = clickedCard.name;
       setTimeout(() => {
+        canClick = true;
         firstCardElement.innerText = '';
         cardElement.innerText = '';
-      }, 1000);
+      }, 3000);
     }
-
-    // reset the first card
     firstCard = null;
   }
 };
@@ -59,7 +47,7 @@ const squareClick = (cardElement, column, row) => {
 const makeDeck = () => {
   // create the empty deck at the beginning
   const newDeck = [];
-  const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+  const suits = ['❤️', '♦️', '♣️', '♠️'];
 
   for (let suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
     // make a variable of the current suit
@@ -73,13 +61,13 @@ const makeDeck = () => {
 
       // 1, 11, 12 ,13
       if (cardName === '1') {
-        cardName = 'ace';
+        cardName = 'A';
       } else if (cardName === '11') {
-        cardName = 'jack';
+        cardName = 'J';
       } else if (cardName === '12') {
-        cardName = 'queen';
+        cardName = 'Q';
       } else if (cardName === '13') {
-        cardName = 'king';
+        cardName = 'K';
       }
 
       // make a single card object variable
@@ -102,8 +90,7 @@ const getRandomIndex = (max) => Math.floor(Math.random() * max);
 
 const shuffleCards = (cardDeck) => {
 // Loop over the card deck array once
-  let currentIndex = 0;
-  while (currentIndex < cardDeck.length) {
+  for (let currentIndex = 0; currentIndex < cardDeck.length; currentIndex += 1) {
     // Select a random index in the deck
     const randomIndex = getRandomIndex(cardDeck.length);
     // Select the card that corresponds to randomIndex
@@ -113,45 +100,27 @@ const shuffleCards = (cardDeck) => {
     // Swap positions of randomCard and currentCard in the deck
     cardDeck[currentIndex] = randomCard;
     cardDeck[randomIndex] = currentCard;
-    // Increment currentIndex to shuffle the next pair of cards
-    currentIndex += 1;
   }
   // Return the shuffled deck
   return cardDeck;
 };
 
-// create all the board elements that will go on the screen
-// return the built board
+// eslint-disable-next-line
 const buildBoardElements = (board) => {
-  // create the element that everything will go inside of
   const boardElement = document.createElement('div');
-
-  // give it a class for CSS purposes
   boardElement.classList.add('board');
 
-  // use the board data structure we passed in to create the correct size board
   for (let i = 0; i < board.length; i += 1) {
-    // make a var for just this row of cards
     const row = board[i];
-
-    // make an element for this row of cards
     const rowElement = document.createElement('div');
     rowElement.classList.add('row');
 
-    // make all the squares for this row
     for (let j = 0; j < row.length; j += 1) {
-      // create the square element
       const square = document.createElement('div');
-
-      // set a class for CSS purposes
       square.classList.add('square');
 
-      // set the click event
       // eslint-disable-next-line
       square.addEventListener('click', (event) => {
-        // we will want to pass in the card element so
-        // that we can change how it looks on screen, i.e.,
-        // "turn the card over"
         squareClick(event.currentTarget, i, j);
       });
 
@@ -159,18 +128,14 @@ const buildBoardElements = (board) => {
     }
     boardElement.appendChild(rowElement);
   }
-
   return boardElement;
 };
 
 const initGame = () => {
-  // create this special deck by getting the doubled cards and
-  // making a smaller array that is ( boardSize squared ) number of cards
   const doubleDeck = makeDeck();
   const deckSubset = doubleDeck.slice(0, boardSize * boardSize);
   deck = shuffleCards(deckSubset);
 
-  // deal the cards out to the board data structure
   for (let i = 0; i < boardSize; i += 1) {
     board.push([]);
     for (let j = 0; j < boardSize; j += 1) {
@@ -179,7 +144,6 @@ const initGame = () => {
   }
 
   const boardEl = buildBoardElements(board);
-
   document.body.appendChild(boardEl);
 };
 
