e9d8bc7acd0bdc2686dc015d517d701f67f1593b
Noel Lim <noellimx@gmail.com>
2022-01-13 01:12:20 +0800

    -

--- script.js
+++ script.js
@@ -1,1 +1,91 @@
-// Please implement exercise logic here
+const SUITS = ["hearts", "diamonds", "clubs", "spades"];
+
+const shuffleCards = (cards) => {
+  const length = cards.length;
+  for (let i = 0; i < length; i += 1) {
+    const j = Math.floor(Math.random() * (length - i)) + i;
+    [cards[i], cards[j]] = [cards[j], cards[i]];
+  }
+  return cards;
+};
+
+const MAX_RANK = 13;
+
+/**
+ *
+ * @param {*} noOfPairs Number of pairs. This method generates a total of (noOfPairs * 2) cards.
+ * @param {*} shuffle Shuffle if true.
+ * @returns
+ */
+const makeShuffledDeck = (noOfPairs, shuffle = true) => {
+  const newDeck = [];
+
+  const lengthSuit = SUITS.length;
+
+  let suitIndex = 0;
+  let pairIndex = 0;
+  while (pairIndex < noOfPairs) {
+    suitIndex += 1;
+    suitIndex %= lengthSuit;
+    const currentSuit = SUITS[suitIndex];
+    const rankCounter = pairIndex % MAX_RANK;
+    let cardName = ``;
+    if (rankCounter === 1) {
+      cardName = `A`;
+    } else if (rankCounter === 11) {
+      cardName = `J`;
+    } else if (rankCounter === 12) {
+      cardName = `👸`;
+    } else if (rankCounter === 0) {
+      cardName = `👑`;
+    } else {
+      cardName = `${rankCounter}`;
+    }
+    const card = {
+      name: cardName,
+      suit: currentSuit,
+      rank: rankCounter,
+    };
+    newDeck.push(card);
+    newDeck.push(card);
+
+    pairIndex += 1;
+  }
+
+  if (shuffle) {
+    shuffleCards(newDeck);
+  }
+
+  return newDeck;
+};
+
+const newCardGrid = (boardSide) => {
+  const height = (width = boardSide);
+  const boardSize = height * width;
+  // boardSize % 2 === 0;
+  const pairs = boardSize / 2;
+  const cards = makeShuffledDeck(pairs);
+  console.log(cards);
+  const board = [];
+  for (let i = 0; i < height; i += 1) {
+    const row = [];
+    for (let j = 0; j < width; j += 1) {
+      row.push(cards.pop());
+    }
+    board.push(row);
+  }
+  return board;
+};
+const initGame = (boardSide) => {
+  const cardGridValues = newCardGrid(boardSide);
+  const cardGrids = cardGridValues.map((value) => ({
+    value,
+    element: newElementCard,
+  }));
+
+  const game = {};
+};
+
+const BOARD_SIDE = 6;
+
+initGame(BOARD_SIDE);
