d1bfb8ea734b648bcd8ab0a13faf8141f68c0956
gracethedragon <gracethedragon@hotmail.com>
2022-01-14 19:33:04 -0800

    added timer

--- script.js
+++ script.js
@@ -1,101 +1,87 @@
 // Please implement exercise logic here
 // Global variables
 // boardSize has to be an even number
+
+// document.body.appendChild(output);
+
 const boardSize = 4;
 const board = [];
 let firstCard = null;
 let firstCardElement;
 let deck;
-let canClick = true
+let canClick = true;
 let matchedCards = 0;
+// create all the board elements that will go on the screen
+// build message output board
+const messageOutput = document.createElement('div');
+messageOutput.classList.add('message');
+document.body.appendChild(messageOutput);
 
-//gameplay
+// gameplay
 const squareClick = (cardElement, column, row) => {
+  console.log(cardElement);
+  console.log('FIRST CARD DOM ELEMENT', firstCard);
+  console.log('BOARD CLICKED CARD', board[row][column]);
+  const clickedCard = board[row][column];
+  if (canClick === false) {
+    return;
+  }
+  // the user already clicked on this square
+  if (cardElement.innerText !== '') {
+    return;
+  }
+  if (firstCard === null) {
+    console.log('first turn');
+    firstCard = clickedCard;
+    // turn this card over
+    cardElement.innerText = `${firstCard.name}\n${firstCard.suit}`;
 
-    console.log(cardElement);
-    
-    console.log('FIRST CARD DOM ELEMENT', firstCard);
-    
-    console.log('BOARD CLICKED CARD', board[row][column]);
-    
-    
-    
-    const clickedCard = board[row][column]; 
-    if(canClick === false){
-      return
-    }
-    // the user already clicked on this square
-    if( cardElement.innerText !== '' ){
+    // hold onto this for later when it may not match
+    firstCardElement = cardElement;
+  // second turn
+  }
+  else
+  {
+    console.log('second turn');
+    cardElement.innerText = `${clickedCard.name}\n${clickedCard.suit}`;
+    if (
+      clickedCard.name === firstCard.name
+      && clickedCard.suit === firstCard.suit
+    ) {
+      matchedCards += 2;
+      if (matchedCards === boardSize * boardSize) {
+        messageOutput.innerText = 'YOU WIN!';
         return;
-    }
-    
-    if (firstCard === null) {
-      console.log('first turn');
-      firstCard = clickedCard;
-      // turn this card over
-      cardElement.innerText = firstCard.name  + '\n' + firstCard.suit;
-  
-      // hold onto this for later when it may not match
-      firstCardElement = cardElement;
-    // second turn
-    } 
-      else 
-      {
-      console.log('second turn');
-      
-      cardElement.innerText = clickedCard.name + '\n' + clickedCard.suit
-      
-      if (
-        clickedCard.name === firstCard.name &&
-        clickedCard.suit === firstCard.suit
-      ) {
-        matchedCards +=2
-
-        if(matchedCards == boardSize*boardSize){
-          messageOutput.innerText = "YOU WIN!"
-          return
-        }
-
-        messageOutput.innerText = "🎉🎉🎉"
-        console.log('match');
-        canClick = false
-        setTimeout(() => {
-          messageOutput.innerText = ""
-          canClick = true
-        }, 3000);
-    
-        // turn this card over
-        cardElement.innerText = clickedCard.name +'\n' + clickedCard.suit;
-        // check that all card elements have inner text
-
-        
-
-      } else {
-        console.log('NOT a match');   
-        
-        canClick = false
-        messageOutput.innerText = "❌❌❌"
-        setTimeout(() => {
-          firstCardElement.innerText = '';
-          cardElement.innerText = ''  
-          messageOutput.innerText = ""
-          canClick = true
-        }, 3000);
-        
       }
-      // reset the first card
-      firstCard = null; 
-      
-}
+
+      messageOutput.innerText = '🎉🎉🎉';
+      console.log('match');
+      canClick = false;
+      setTimeout(() => {
+        messageOutput.innerText = '';
+        canClick = true;
+      }, 3000);
+
+      // turn this card over
+      cardElement.innerText = `${clickedCard.name}\n${clickedCard.suit}`;
+      // check that all card elements have inner text
+    } else {
+      console.log('NOT a match');
+
+      canClick = false;
+      messageOutput.innerText = '❌❌❌';
+      setTimeout(() => {
+        firstCardElement.innerText = '';
+        cardElement.innerText = '';
+        messageOutput.innerText = '';
+        canClick = true;
+      }, 3000);
+    }
+    // reset the first card
+    firstCard = null;
+  }
 };
 
-
-// create all the board elements that will go on the screen
-//build message output board
-  const messageOutput = document.createElement ('div')
-  messageOutput.classList.add("message")
-  document.body.appendChild(messageOutput)
-  
 // return the built board
 const buildBoardElements = (board) => {
   // create the element that everything will go inside of
@@ -128,26 +114,22 @@ const buildBoardElements = (board) => {
         // that we can change how it looks on screen, i.e.,
         // "turn the card over"
         squareClick(event.currentTarget, i, j);
-
-        
       });
 
       rowElement.appendChild(square);
     }
     boardElement.appendChild(rowElement);
   }
-  
+
   return boardElement;
 };
 
-
-
-//game initialisation
+// game initialisation
 const initGame = () => {
   // create this special deck by getting the doubled cards and
   // making a smaller array that is ( boardSize squared ) number of cards
-  let doubleDeck = makeDeck();
-  let deckSubset = doubleDeck.slice(0, boardSize * boardSize);
+  const doubleDeck = makeDeck();
+  const deckSubset = doubleDeck.slice(0, boardSize * boardSize);
   deck = shuffleCards(deckSubset);
 
   // deal the cards out to the board data structure
@@ -159,14 +141,11 @@ const initGame = () => {
   }
 
   const boardEl = buildBoardElements(board);
-
   document.body.appendChild(boardEl);
-  
 };
 
 // Get a random index ranging from 0 (inclusive) to max (exclusive).
 const getRandomIndex = (max) => Math.floor(Math.random() * max);
-
 
 // Shuffle an array of cards
 const shuffleCards = (cards) => {
@@ -186,8 +165,8 @@ const shuffleCards = (cards) => {
   return cards;
 };
 
-//Make deck
-//const makeDeck = (cardAmount)
+// Make deck
+// const makeDeck = (cardAmount)
 const makeDeck = () => {
   // create the empty deck at the beginning
   const newDeck = [];
@@ -228,11 +207,41 @@ const makeDeck = () => {
       newDeck.push(card); // add double the cards to the deck
       newDeck.push(card);
     }
-    
   }
   return newDeck;
 };
 
+let milliseconds = 1 * 60 * 1000;
 
-window.addEventListener('load', initGame)
+// console.log(totalSeconds);
+// console.log(minutes);
+// console.log(seconds);
+
+const delayInMilliseconds = 1000;
+const output = document.createElement('div');
+output.id = 'timer';
+output.innerText = 'Let\'s go!';
+document.body.appendChild(output);
+
+const ref = setInterval(() => {
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds - minutes * 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  output.innerText = `${minutes}:${seconds} `;
+  console.log(milliseconds);
+
+  if (milliseconds <= 0) {
+    clearInterval(ref);
+    messageOutput.innerText = 'GAME OVER!';
+    canClick = false;
+    return;
+  }
+
+  milliseconds -= 1000;
+}, delayInMilliseconds);
+window.addEventListener('load', initGame);
 // crossOriginIsolated.log(cardAmount)
